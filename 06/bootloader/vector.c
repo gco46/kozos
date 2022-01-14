4815efac1d8d52c91f11058959d2db44e2342c09
kojiro tanaka <kojiro.tanaka0406@gmail.com>
2022-01-14 16:09:32 +0900

    implement os mock

---
+++ 06/bootloader/vector.c
@@ -0,0 +1,15 @@
+#include "defines.h"
+
+extern void start(void);
+
+// 割り込みベクタの設定
+void (*vectors[])(void) = {
+	start, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 
+};
